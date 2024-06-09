7be79a76077abbf22e85b0b2842cab02868f09d2
VKTRenokh <viktorenokh@gmail.com>
2024-06-09 10:28:37 +0300

    feat: add jsdoc for isExternalSignal function

--- packages/signals/src/utils.ts
+++ packages/signals/src/utils.ts
@@ -10,6 +10,14 @@ function hasBrand(value: object) {
   return 'brand' in value && typeof value.brand === 'symbol';
 }
 
+/**
+ * Determines whether a given value is an external signal from another build.
+ *
+ * This function is intended for use in scenarios where you have a clear
+ * understanding of the value's context and type. It checks if the value is an
+ * object, is not null or undefined, and possesses a specific branding property
+ * indicating it is an external signal
+ */
 export function isExternalSignal<T>(value: T | Signal<T>): value is Signal<T> {
   return typeof value === 'object' && isNotNullable(value) && hasBrand(value);
 }
